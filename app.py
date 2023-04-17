2229f9a1b7f9ffb8994b2c326c956255f3a66a38
Andrea <aka.spasojevic@gmail.com>
2023-04-17 12:57:07 +0200

    version 2.0

--- app.py
+++ app.py
@@ -9,16 +9,11 @@ app = Flask(__name__) #initialising flask
 def home():
     return render_template("home.html") #rendering our home.html contained within /templates
 
-@app.route("/account", methods=["POST", "GET"]) #defining the routes for the account() funtion
-def account():
-    usr = "<User Not Defined>" #Creating a variable usr
-    if (request.method == "POST"): #Checking if the method of request was post
-        usr = request.form["name"] #getting the name of the user from the form on home page
-        if not usr: #if name is not defined it is set to default string
-            usr = "<User Not Defined>"
-    return render_template("actions.html",username=usr) #rendering our actions.html contained within /templates
+@app.route("/actions") #defining the routes for the actions funtion
+def actions():
+    return render_template("actions.html") #rendering our actions.html contained within /templates
 
 
 
-if __name__ == "__main__": #checking if __name__'s value is '__main__'. __name__ is an python environment variable who's value will always be '__main__' till this is the first instatnce of app.py running
+if __name__ == "__main__":
     app.run(debug=True,port=4949) #running flask (Initalised on line 4)
