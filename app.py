9ecccdc33c36b417a5a03baae221697797be709e
Andrea <aka.spasojevic@gmail.com>
2023-04-18 23:33:53 +0200

    version 2.0

--- app.py
+++ app.py
@@ -1,3 +1,6 @@
+import os
+import sys
+
 from flask import * #importing flask (Install it using python -m pip install flask)
 
 
@@ -13,7 +16,13 @@ def home():
 def actions():
     return render_template("actions.html") #rendering our actions.html contained within /templates
 
+@app.route("/read") #defining the routes for the actions funtion
+def read():
+    return render_template("read.html") #rendering our actions.html contained within /templates
 
+@app.route("/play") #defining the routes for the actions funtion
+def play():
+    return render_template("play.html") #rendering our actions.html contained within /templates
 
 if __name__ == "__main__":
     app.run(debug=True,port=4949) #running flask (Initalised on line 4)
